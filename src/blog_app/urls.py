1bdef71edcfd94b95154045d9a1efb8132a9d0ac
Mohebul Hasan Emon <158182078+mohebulh@users.noreply.github.com>
2025-01-08 22:33:31 +0000

    Delete src directory
    deleate old file

--- src/blog_app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from blog_app import views
-
-urlpatterns = [
-    path('',views.blog, name='blog'),
-]
--- src/home_app/urls.py
+++
@@ -1,5 +0,0 @@
-from home_app import views
-from django.urls import path
-urlpatterns = [
-    path('',views.home, name='home'),
-]
